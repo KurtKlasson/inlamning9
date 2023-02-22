6920b5ba6c42ce9a772dce2d160c71d6a1165359
Calle <derpninjas88rgmail.com>
2023-02-22 15:08:31 +0100

    miscom

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
